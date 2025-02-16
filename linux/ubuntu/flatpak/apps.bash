9c0268846ea98355f97d4fef9b3b9ce89e746d63
Effi Ovadia <effi.ovadia@gmail.com>
2025-02-16 04:48:49 +0200

    renamed:    linux/ubuntu/flatpak/applications.bash -> linux/ubuntu/flatpak/apps.bash

--- linux/ubuntu/flatpak/apps.bash
+++ linux/ubuntu/flatpak/apps.bash
@@ -2,10 +2,10 @@
 
 ## Some usefull FlatPak apps to install
 flatpak install flathub -y us.zoom.Zoom
-flatpak install flathub -y com.wps.Office
-flatpak install flathub -y com.slack.Slack
+#flatpak install flathub -y com.wps.Office
+#flatpak install flathub -y com.slack.Slack
 flatpak install flathub -y com.microsoft.Teams
-flatpak install flathub -y com.anydesk.Anydesk
+#flatpak install flathub -y com.anydesk.Anydesk
 flatpak install flathub -y com.discordapp.Discord
 flatpak install flathub -y com.belmoussaoui.Authenticator
 flatpak install flathub -y io.github.mimbrero.WhatsAppDesktop
@@ -16,3 +16,5 @@ flatpak install flathub -y com.bitwarden.desktop
 flatpak install flathub -y md.obsidian.Obsidian
 flatpak install flathub -y com.skype.Client
 flatpak install flathub -y com.adobe.Reader
+flatpak install flathub -y com.tencent.WeChat
+flatpak install flathub -y com.viber.Viber
