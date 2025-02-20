e78cf22e6670313870653f774c98060892870248
Effi Ovadia <effi.ovadia@gmail.com>
2025-02-20 02:42:43 +0200

    modified:   bsd/vmware_display.bash

--- bsd/vmware_display.bash
+++ bsd/vmware_display.bash
@@ -2,7 +2,7 @@
 
 systemctl restart vmtoolsd
 
-sed -i 's/[# ]*MODULES=.*/MODULES=(vsock vmw_vsock_vmci_transport vmw_balloon vmw_vmci vmwgfx)/g' \
-  /etc/mkinitcpio.conf
+sed -i \
+  's/[# ]*MODULES=.*/MODULES=(vsock vmw_vsock_vmci_transport vmw_balloon vmw_vmci vmwgfx)/g' /etc/mkinitcpio.conf
           
 mkinitcpio -P
