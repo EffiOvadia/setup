58ec28dde1e36b9abf93e3542250614ce73deabe
Effi Ovadia <effi.ovadia@gmail.com>
2025-03-11 18:57:09 +0200

    modified:   linux/ubuntu/apps/appimage/setup.bash 	modified:   linux/ubuntu/apps/flatpak/setup.bash

--- linux/ubuntu/apps/appimage/setup.bash
+++ linux/ubuntu/apps/appimage/setup.bash
@@ -2,3 +2,25 @@
 
 sudo add-apt-repository universe
 sudo apt install libfuse2t64
+
+
+## example ::
+
+# Install Joplin via the AppImage (no official repository)
+wget -O joplin.AppImage https://github.com/laurent22/joplin/releases/latest/download/Joplin-x86_64.AppImage
+chmod +x joplin.AppImage
+
+# Move to a better location
+sudo mkdir -p /opt/joplin
+sudo mv joplin.AppImage /opt/joplin/Joplin.AppImage
+
+# Create desktop shortcut
+cat << EOF | sudo tee /usr/share/applications/joplin.desktop
+[Desktop Entry]
+Name=Joplin
+Comment=Joplin note-taking app
+Exec=/opt/joplin/Joplin.AppImage
+Icon=joplin
+Type=Application
+Categories=Office;
+EOF
--- linux/ubuntu/apps/flatpak/setup.bash
+++ linux/ubuntu/apps/flatpak/setup.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Add FlatPak support
-install flatpak gnome-software-plugin-flatpak
+install flatpak gnome-software-plugin-flatpak 
 # Add the Flathub repository
 flatpak remote-delete --force flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
