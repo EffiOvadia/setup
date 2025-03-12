92b26442eaec2e7a7930999e769b06e9ba3f4f25
Effi Ovadia <effi.ovadia@gmail.com>
2025-03-12 15:32:18 +0200

    modified:   linux/ubuntu/apps/apps(deb).bash

--- linux/ubuntu/apps/apps(deb).bash
+++ linux/ubuntu/apps/apps(deb).bash
@@ -113,7 +113,8 @@ apps=( fonts-sil-ezra fonts-sil-lateef fonts-sil-scheherazade fonts-sil-andika \
   ttf-bitstream-vera fonts-agave fonts-roboto fonts-noto ttf-mscorefonts-installer \
   fonts-cascadia-code fonts-open-sans fonts-freefont-ttf fonts-liberation2 \
   fonts-dejavu fonts-jetbrains-mono fonts-mononoki fonts-inter fonts-noto-cjk \
-  culmus typecatcher fonts-source-code-pro fonts-sil-gentium )
+  culmus typecatcher fonts-sil-gentium )
+  #fonts-source-code-pro
 install ${apps[@]}
 apps=( openvpn wireguard network-manager-openconnect-gnome openconnect geary \
   filezilla qbittorrent deluge transmission )
