5e938e2b94141435e3558f543b7516f6f9c3413c
Effi Ovadia <effi.ovadia@gmail.com>
2025-03-25 10:40:26 +0200

    modified:   windows/CLI setup/winget/development.ps1 	modified:   windows/CLI setup/winget/docs & images.ps1

--- windows/CLI setup/winget/docs & images.ps1
+++ windows/CLI setup/winget/docs & images.ps1
@@ -10,7 +10,10 @@ $Apps =
   #[PSCustomObject]@{Name='Adobe Acrobat Reader'; ID='Adobe.Acrobat.Reader.64-bit'}
   [PSCustomObject]@{Name='Clipchamp'; ID='Clipchamp.Clipchamp_yxz26nhyzhsrt'}
   [PSCustomObject]@{Name='Adobe DNG Converter'; ID='Adobe.DNGConverter'}
+  [PSCustomObject]@{Name='OnlyOffice'; ID='ONLYOFFICE.DesktopEditors'}
   [PSCustomObject]@{Name='IrfanView'; ID='IrfanSkiljan.IrfanView'}
+  [PSCustomObject]@{Name='Obsidian'; ID='Obsidian.Obsidian'}
+  [PSCustomObject]@{Name='Joplin'; ID='Joplin.Joplin'}
   [PSCustomObject]@{Name='Inkscape'; ID='9PD9BHGLFC7H'}
   #[PSCustomObject]@{Name='Gimp'; ID='XPDM27W10192Q0'}
   )
