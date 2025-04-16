937a1f5c92bee16adfd2e6e5680ef92826a88e13
Effi Ovadia <effi.ovadia@gmail.com>
2025-04-16 22:03:33 +0300

    modified:   linux/ubuntu/apps/flatpak/apps.bash

--- linux/ubuntu/apps/flatpak/apps.bash
+++ linux/ubuntu/apps/flatpak/apps.bash
@@ -6,15 +6,21 @@ sudo flatpak install flathub -y com.wps.Office                      # WPS Office
 sudo flatpak install flathub -y com.slack.Slack                     # Slack
 sudo flatpak install flathub -y com.microsoft.Teams                 # Microsoft Teams
 sudo flatpak install flathub -y io.github.sxyazi.yazi               # Yazi filemanager
+sudo flatpak install flathub -y org.chromium.Chromium               # Chromium
 sudo flatpak install flathub -y eu.betterbird.Betterbird            # Betterbird
 sudo flatpak install flathub -y org.localsend.localsend_app         # LocalSend 
 sudo flatpak install flathub -y io.gitlab.librewolf-community       # LibreWolf
 sudo flatpak install flathub -y io.github.mimbrero.WhatsAppDesktop  # WhatsApp 
+sudo flatpak install flathub -y org.raspberrypi.rpi-imager          # Raspberry Pi Imager
 sudo flatpak install flathub -y io.github.shiftey.Desktop           # GitHub Desktop
+sudo flatpak install flathub -y com.github.tchx84.Flatseal          # Flatseal
+sudo flatpak install flathub -y net.cozic.joplin_desktop            # Joplin
 sudo flatpak install flathub -y com.discordapp.Discord              # Discord
 sudo flatpak install flathub -y net.waterfox.waterfox               # Waterfox
 sudo flatpak install flathub -y com.bitwarden.desktop               # Bitwarden
 sudo flatpak install flathub -y md.obsidian.Obsidian                # Obsidian
 sudo flatpak install flathub -y com.tencent.WeChat                  # WeChat
+sudo flatpak install flathub -y me.proton.Mail                      # ProtonMail 
 sudo flatpak install flathub -y org.gnome.Boxes                     # GNOME Boxes
+sudo flatpak install flathub -y com.tutanota.Tutanota               # Tutanota
 sudo flatpak install flathub -y io.github.thetumultuousunicornofdarkness.cpu-x # CPU-X
