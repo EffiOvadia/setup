81885de3f0f49c4d8ef23262ea8b116e5c984d93
Effi Ovadia <effi.ovadia@gmail.com>
2025-05-01 15:03:08 +0300

    modified:   linux/ubuntu/apps/flatpak/apps.bash 	modified:   linux/ubuntu/apps/flatpak/setup.bash

--- linux/ubuntu/apps/flatpak/apps.bash
+++ linux/ubuntu/apps/flatpak/apps.bash
@@ -1,30 +1,26 @@
 #!/usr/bin/env bash
 
 ## Some usefull FlatPak apps to install
-sudo flatpak install -y com.rustdesk.RustDesk               # RustDesk
-sudo flatpak install -y org.localsend.localsend_app         # LocalSend 
+sudo flatpak install -y com.rustdesk.RustDesk                 # RustDesk
+sudo flatpak install -y org.localsend.localsend_app           # LocalSend 
+sudo flatpak install -y com.github.eneshecan.WhatsAppForLinux # WhatsApp
 
-sudo flatpak install -y us.zoom.Zoom                        # Zoom
-sudo flatpak install -y com.wps.Office                      # WPS Office
-sudo flatpak install -y com.slack.Slack                     # Slack
-sudo flatpak install -y com.microsoft.Teams                 # Microsoft Teams
-sudo flatpak install -y io.github.sxyazi.yazi               # Yazi filemanager
-sudo flatpak install -y org.chromium.Chromium               # Chromium
-sudo flatpak install -y eu.betterbird.Betterbird            # Betterbird
-sudo flatpak install -y io.gitlab.librewolf-community       # LibreWolf
-sudo flatpak install -y org.raspberrypi.rpi-imager          # Raspberry Pi Imager
-sudo flatpak install -y com.github.tchx84.Flatseal          # Flatseal
-sudo flatpak install -y net.cozic.joplin_desktop            # Joplin
-sudo flatpak install -y com.discordapp.Discord              # Discord
-sudo flatpak install -y net.waterfox.waterfox               # Waterfox
-sudo flatpak install -y com.bitwarden.desktop               # Bitwarden
-sudo flatpak install -y md.obsidian.Obsidian                # Obsidian
-sudo flatpak install -y com.tencent.WeChat                  # WeChat
-sudo flatpak install -y me.proton.Mail                      # ProtonMail 
-sudo flatpak install -y org.gnome.Boxes                     # GNOME Boxes
-sudo flatpak install -y com.tutanota.Tutanota               # Tutanota
-
-
-#sudo flatpak install -y io.github.mimbrero.WhatsAppDesktop  # WhatsApp
-#sudo flatpak install -y io.github.shiftey.Desktop           # GitHub Desktop
-sudo flatpak info com.rustdesk.RustDesk
+sudo flatpak install -y us.zoom.Zoom                          # Zoom
+sudo flatpak install -y com.slack.Slack                       # Slack
+sudo flatpak install -y com.microsoft.Teams                   # Microsoft Teams
+sudo flatpak install -y io.github.sxyazi.yazi                 # Yazi filemanager
+sudo flatpak install -y org.chromium.Chromium                 # Chromium
+sudo flatpak install -y eu.betterbird.Betterbird              # Betterbird
+sudo flatpak install -y io.gitlab.librewolf-community         # LibreWolf
+sudo flatpak install -y org.raspberrypi.rpi-imager            # Raspberry Pi Imager
+sudo flatpak install -y com.github.tchx84.Flatseal            # Flatseal
+sudo flatpak install -y net.cozic.joplin_desktop              # Joplin
+sudo flatpak install -y com.discordapp.Discord                # Discord
+sudo flatpak install -y net.waterfox.waterfox                 # Waterfox
+sudo flatpak install -y com.bitwarden.desktop                 # Bitwarden
+sudo flatpak install -y md.obsidian.Obsidian                  # Obsidian
+sudo flatpak install -y com.tencent.WeChat                    # WeChat
+sudo flatpak install -y me.proton.Mail                        # ProtonMail 
+sudo flatpak install -y org.gnome.Boxes                       # GNOME Boxes
+sudo flatpak install -y com.tutanota.Tutanota                 # Tutanota
+sudo flatpak install -y com.wps.Office                        # WPS Office
