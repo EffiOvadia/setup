3cc4d3a71f1203af1c8febb2e1b544e29de3b22a
Effi Ovadia <effi.ovadia@gmail.com>
2025-05-02 01:31:06 +0300

    modified:   routers/merlin/enable_swap.sh

--- routers/merlin/enable_swap.sh
+++ routers/merlin/enable_swap.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
-entware="/dev/sda1"
-swap="/dev/sda2"
+DEV=/dev/sda
 
 mkdir -p /tmp/entware
 
-mkswap $swap
-swapon $swap && free
+mkfs.ext4 ${DEV}1 && 
+  mount -t ext4 ${DEV}1 /tmp/entware
+
+mkswap ${DEV}2 && 
+  swapon ${DEV}2
 
 script=/jffs/scripts/post-mount
 
 [ -f "$script" ] || touch "$script"
 
-grep -qFx "mount -t ext4 $entware /tmp/ssd" "$script" || 
-  echo "mount -t ext4 $entware /tmp/ssd" >> "$script"
+grep -qFx "mount -t ext4 ${DEV}1 /tmp/ssd" "$script" || 
+  echo "mount -t ext4 ${DEV}1 /tmp/ssd" >> "$script"
 
-grep -qFx "swapon $swap" "$script" || 
-  echo "swapon $swap" >> "$script"
+grep -qFx "swapon ${DEV}2" "$script" || 
+  echo "swapon ${DEV}2" >> "$script"
 
 chmod +x $script
