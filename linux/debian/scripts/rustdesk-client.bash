40d5fea56aa9b3ac7e0dee89bffb9b1ce02d126c
Effi Ovadia <effi.ovadia@gmail.com>
2025-08-22 01:23:53 +0300

    modified:   linux/debian/scripts/rustdesk-client.bash

--- linux/debian/scripts/rustdesk-client.bash
+++ linux/debian/scripts/rustdesk-client.bash
@@ -7,22 +7,22 @@ sudo mkdir -p ~/.var/app/com.rustdesk.RustDesk/config/rustdesk
 # Write the configuration
 sudo cat <<EOF | sudo tee ~/.var/app/com.rustdesk.RustDesk/config/rustdesk/RustDesk2.toml ~/.config/rustdesk/RustDesk2.toml
 rendezvous_server = '172.232.211.116:21116'
+trusted_devices = '00aLG2qnvIwvV1ja/SnjbPpQL8'
 nat_type = 1
 serial = 0
 unlock_pin = ''
 
 [options]
-av1-test = 'Y'
-direct-server = 'Y'
-access-mode = 'full'
-allow-auto-disconnect = 'Y'
-auto-disconnect-timeout = '10'
 relay-server = '172.232.211.116'
 custom-rendezvous-server = '172.232.211.116'
 key = 'FGnqzqGE19xjRrIkoWCoLTOfKJtKh06dLYRmnzOjJRs='
 verification-method = 'use-permanent-password'
-allow-remove-wallpaper = 'Y'
+auto-disconnect-timeout = '5'
+allow-auto-disconnect = 'Y'
 allow-linux-headless = 'Y'
+access-mode = 'full'
+direct-server = 'Y'
+av1-test = 'Y'
 EOF
 
 [ -f /etc/gdm3/custom.conf ] && sudo sed -i 's/[# ]*WaylandEnable=.*/WaylandEnable=false/g' /etc/gdm3/custom.conf ||
